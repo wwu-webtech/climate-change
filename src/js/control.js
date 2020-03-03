25ace71a5c6ec37877da5c49da6745155c89373e
masons7 <masons7@wwu.edu>
2020-03-03 14:03:21 -0800

    navigation added

--- src/js/control.js
+++ src/js/control.js
@@ -1,7 +1,11 @@
 var animationsOn = true;
 var animationToggle = document.getElementById('animationToggle');
 var body = document.getElementById('climate-change');
+var html = document.getElementsByTagName('html');
 var aosElements = document.getElementsByClassName('aos-init');
+
+var navWrapper = document.getElementsByClassName('chapter-navigation')[0];
+var chapterToggle = document.getElementsByClassName('chapter-navigation-toggle')[0];
 
 animationToggle.onclick = function() {
 
@@ -15,7 +19,9 @@ animationToggle.onclick = function() {
     animationState.innerText = "disabled";
 
     body.classList.add('animations-off');
+    html[0].classList.add('animations-off');
     body.classList.remove('animations-on');
+    html[0].classList.remove('animations-on');
 
     for (each of aosElements) {
       each.removeAttribute('data-aos');
@@ -34,6 +40,23 @@ var isInViewport = function (elem) {
     bounding.top <= halfway
   );
 };
+
+chapterToggle.onclick = function() {
+  if (chapterToggle.classList.contains('closed')) {
+    navWrapper.classList.remove('closed');
+    navWrapper.classList.add('open');
+    chapterToggle.classList.remove('closed');
+    chapterToggle.classList.add('open');
+    chapterToggle.innerHTML = '<span class="material-icons" aria-hidden="true">menu_open</span><span class="button-text visually-hidden">Close the chapter navigation menu</span>';
+  }
+  else {
+    navWrapper.classList.remove('open');
+    navWrapper.classList.add('closed');
+    chapterToggle.classList.remove('open');
+    chapterToggle.classList.add('closed');
+    chapterToggle.innerHTML = '<span class="material-icons" aria-hidden="true">menu</span><span class="button-text visually-hidden">Open the chapter navigation menu</span>';
+  }
+}
 
 /* trigger ice core animation */
 
